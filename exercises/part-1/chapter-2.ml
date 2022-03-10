4d83e9d400cb4db237e5c06d004888501736e217
Anton Macmillan-Parks <anton.macmillanparks@clearscore.com>
2022-03-10 10:12:59 +0000

    Update second exercise

--- exercises/part-1/chapter-2.ml
+++ exercises/part-1/chapter-2.ml
@@ -14,15 +14,20 @@ let slow_add2 n = Unix.sleep 10; n + 2
 
 let slow_add2_memo = memoize slow_add2
 
-let print_add2_result = 
-  print_string ("Add 2 - First call complete - result was: " ^ (string_of_int (slow_add2_memo 2)) ^ "\n");
-  print_string ("Add 2 - Second call complete - result was: " ^ (string_of_int (slow_add2_memo 2)) ^ "\n")
-
 let random_from_seed n = 
   Random.init n;
   Random.bits ()
 
 let random_memo = memoize random_from_seed
+
+let id : bool -> bool = fun a -> a
+let not: bool -> bool = fun a -> if a then false else true
+let const_true: bool -> bool = fun _ -> true
+let const_false: bool -> bool = fun _ -> false
+
+let print_add2_result = 
+  print_string ("Add 2 - First call complete - result was: " ^ (string_of_int (slow_add2_memo 2)) ^ "\n");
+  print_string ("Add 2 - Second call complete - result was: " ^ (string_of_int (slow_add2_memo 2)) ^ "\n")
 
 let print_random_result =
   print_string ("Random result with seed of 1: " ^ (string_of_int (random_memo 1)) ^ "\n");
