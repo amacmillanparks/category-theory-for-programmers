a0ba59ef4556f038808512319cf25302725dde48
Anton Macmillan-Parks <anton.macmillanparks@clearscore.com>
2022-03-10 01:56:11 +0000

    Add second exercise

---
+++ exercises/part-1/chapter-2.ml
@@ -0,0 +1,32 @@
+#load "unix.cma";;
+
+let memoize f =
+  let ht = Hashtbl.create 1 in
+  fun a ->
+    match Hashtbl.find_opt ht a with
+    | Some(b) -> b
+    | None ->
+      let b = f a in
+      Hashtbl.add ht a b;
+      b
+
+let slow_add2 n = Unix.sleep 10; n + 2
+
+let slow_add2_memo = memoize slow_add2
+
+let print_add2_result = 
+  print_string ("Add 2 - First call complete - result was: " ^ (string_of_int (slow_add2_memo 2)) ^ "\n");
+  print_string ("Add 2 - Second call complete - result was: " ^ (string_of_int (slow_add2_memo 2)) ^ "\n")
+
+let random_from_seed n = 
+  Random.init n;
+  Random.bits ()
+
+let random_memo = memoize random_from_seed
+
+let print_random_result =
+  print_string ("Random result with seed of 1: " ^ (string_of_int (random_memo 1)) ^ "\n");
+  print_string ("Random result with seed of 2: " ^ (string_of_int (random_memo 2)) ^ "\n");
+  print_string ("Random result with seed of 2: " ^ (string_of_int (random_memo 2)) ^ "\n")
+
+let () = print_add2_result; print_random_result 
